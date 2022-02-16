2539340bbf9f6bdb20d395afe7f01a7ec5fce5e5
resmile <resmile@gmail.com>
2022-02-17 03:26:59 +0900

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "smile": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
